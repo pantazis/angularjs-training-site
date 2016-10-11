7803457186455bd669c477e28c5e00d629170dba
pantazis <pantazisvastardis@yahoo.com>
2016-10-11 10:52:31 +0300

    xx

---
+++ directives/mainDirective.js
@@ -0,0 +1,47 @@
+var myApp = angular.module("app", ['ui.router']);
+
+
+
+myApp.controller('httpreqest', function ($scope,$http) {
+    $http.get("../assets/json/article.json")
+    .then(function(response) {
+        $scope.myWelcome = response.data;
+        console.log(response.data);
+    });
+});
+myApp.controller('gallery', function ($scope,$http) {
+    $http.get("../assets/json/gallery.json")
+    .then(function(response) {
+        $scope.mygallery = response.data;
+        console.log(response.data);
+    });
+});
+
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: 'views/home.html'
+        })
+        .state('about', {
+            url: "/about",
+            templateUrl: 'views/profile.html'
+        })
+      .state('nea', {
+            url: "/nea",
+            templateUrl: 'views/news.html',
+            controller: 'httpreqest'
+        })
+        .state('epikinonia', {
+            url: "/epikinonia",
+            templateUrl: 'views/contact.html'
+        })
+        .state('Gallery', {
+            url: "/Gallery",
+            templateUrl: 'views/Gallery.html',
+            controller: 'gallery'
+        });
+
+});
